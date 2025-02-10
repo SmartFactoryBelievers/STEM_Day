003bab5f43a5499982f05265768f595a7dee6fcd
Smart Factory Believers <128001053+SmartFactoryBelievers@users.noreply.github.com>
2025-02-10 15:28:07 -0600

    Update Read_ArUco.py

--- ArUco_Project/Examples/Read_ArUco.py
+++ ArUco_Project/Examples/Read_ArUco.py
@@ -1,57 +1,5 @@
-import RPi.GPIO as GPIO
 import numpy as np
 from ArUcoReader import ArUcoDetector
-
-# Define the GPIO pins
-Left_Forward_Pin = 35
-Left_Backward_Pin = 31
-Right_Forward_Pin = 26
-Right_Backward_Pin = 21
-
-#Setting up our pins
-GPIO.setmode(GPIO.BOARD)
-#Our output pins, start off
-GPIO.setup(Left_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Left_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Forward_Pin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Right_Backward_Pin, GPIO.OUT, initial=GPIO.LOW)
-
-# Movement Functions
-def forward():
-    print("Moving Forward")
-    GPIO.output(Left_Forward_Pin, GPIO.HIGH)
-    GPIO.output(Left_Backward_Pin, GPIO.LOW)
-    GPIO.output(Right_Forward_Pin, GPIO.HIGH)
-    GPIO.output(Right_Backward_Pin, GPIO.LOW)
-
-def backward():
-    print('backward')
-    GPIO.output(Left_Backward_Pin, GPIO.HIGH) #Left motor backward
-    GPIO.output(Right_Backward_Pin, GPIO.HIGH) #Right motor backward
-    GPIO.output(Left_Backward_Pin, GPIO.LOW) #Left motor off
-    GIO.output(Right_Backward_Pin, GPIO.LOW) #Right motor off
-    
-
-def turn_left():
-    print("Turning Left")
-    GPIO.output(Left_Forward_Pin, GPIO.LOW)
-    GPIO.output(Left_Backward_Pin, GPIO.HIGH)
-    GPIO.output(Right_Forward_Pin, GPIO.HIGH)
-    GPIO.output(Right_Backward_Pin, GPIO.LOW)
-
-def turn_right():
-    print("Turning Right")
-    GPIO.output(Left_Forward_Pin, GPIO.HIGH)
-    GPIO.output(Left_Backward_Pin, GPIO.LOW)
-    GPIO.output(Right_Forward_Pin, GPIO.LOW)
-    GPIO.output(Right_Backward_Pin, GPIO.HIGH)
-
-def stop():
-    print("Stopping")
-    GPIO.output(Left_Forward_Pin, GPIO.LOW)
-    GPIO.output(Left_Backward_Pin, GPIO.LOW)
-    GPIO.output(Right_Forward_Pin, GPIO.LOW)
-    GPIO.output(Right_Backward_Pin, GPIO.LOW)
 
 if __name__ == "__main__":
     # Load calibration data
